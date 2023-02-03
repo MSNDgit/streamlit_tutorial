41a29da1f3d0af019f26d27b4f5b7f08d8d801c1
MSNDigit <powerover87@gmail.com>
2023-02-03 13:23:28 +0900

    commit2

--- first_app.py
+++ first_app.py
@@ -5,15 +5,43 @@
 import streamlit as st
 import pandas as pd
 
+def text():
+    #Mark Down
+    st.markdown('Markdown')
+    st.markdown('Streamlit is **_really_ cool**.')
+    st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
+    st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
+
+def dataframe1():
+    df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+    st.dataframe(df) # Same as st.write(df)
+
+def temp_map():
+    # 온도 출력
+    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+    # 위도 경도에 맞는 지도를 출력
+    df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+    st.map(df)
+
+
 def main():
     st.title("First_APP")
-
-    df = pd.Dataframe({
-        "first_col":[1,2,3,4],
-        "second_col":[10,20,30,40]
+    df = pd.DataFrame({
+        "first_col":[1, 2, 3, 4],
+        "second_col":[10, 20, 30, 40]
     })
-print("my name is {_main_}")
+    st.write(df)
 
-st.write(df)
-if __name__ == "__main__"
+    text()
+
+    if st.checkbox("show dataframe"):
+        dataframe1()
+
+    temp_map()
+
+if __name__ == "__main__":
     main()
